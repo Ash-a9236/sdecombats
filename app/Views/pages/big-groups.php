0b958c96a7c57f9901518ca916ed96eac1caf233
root <root@H.ash_a9236.org>
2025-12-11 12:31:27 -0500

    adding missing pages

--- app/Views/pages/big-groups.php
+++ app/Views/pages/big-groups.php
@@ -1,1 +1,13 @@
 <?php
+
+use App\Helpers\UserContext;
+use App\Helpers\ViewHelper;
+
+ViewHelper ::loadHeader('');
+
+$user = UserContext ::getCurrentUser();
+
+?>
+
+<div id="home-wrapper">
+</div>
